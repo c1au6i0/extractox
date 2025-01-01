008a189573c0e249c1cbc395cf9ea50a9a7da1dd
c1au6i0 <claudio.zanettini@gmail.com>
2025-01-01 14:42:19 -0500

    enh: tests for cache functions [skipci]

--- tests/testthat/test-chace.R
+++ tests/testthat/test-chace.R
@@ -3,35 +3,79 @@ library(fs)
 
 # Check setup.R
 
-
-
-# message(tools::R_user_dir("extractox", which = "cache"))
+# @@@@@@@@@@@@@@@@
+# Save Cache ----
+# @@@@@@@@@@@@@@@@
 
 song <- c("bella", "ciao", "bella", "ciao", "ciao", "ciao")
 
 test_that("Save to cache works", {
 
-
-    expect_message({
-      file_path <- save_to_cache(dat = song, file_name = "song", verbose =  TRUE)
+   expect_message({
+      file_path <- save_to_cache(dat = song, file_name = "song.txt", verbose = TRUE)
     }, "Saving")
 
-
-    lapply()
-
-   exp_path  <- fs::path(Sys.getenv("R_USER_CACHE_DIR"), "song")
-   message("Expected ", exp_path, "\n")
-
+   exp_path  <- normalizePath(fs::path(Sys.getenv("R_USER_CACHE_DIR"), "R",
+                                       "extractox", "song.txt"))
    file_path <- normalizePath(file_path)
-   message("Real ", file_path)
+
+   expect_equal(exp_path, file_path)
+   message(file_path)
+})
+
+test_that("Save to cache overwrites cache if present", {
+  expect_message({
+    file_path <- save_to_cache(dat = song, file_name = "song.txt", verbose =  TRUE)
+  }, "Overwriting")
+
+  expect_no_message({
+    file_path <- save_to_cache(dat = song,
+                               file_name = "song.txt",
+                               verbose = FALSE)
+  })
+})
+
+# @@@@@@@@@@@@@@@@
+# Load Cache ----
+# @@@@@@@@@@@@@@@@
+
+test_that("load cache verbose, load silent", {
 
 
-#   expect_true(is.list(out))
-#   expect_true(all(unlist(lapply(out, is.data.frame))))
-#   expect_equal(nrow(out$comptox_main_data), 1)
-#   expect_equal(nrow(out$comptox_main_data), 1)
-#   expect_equal(nrow(out$comptox_cover_sheet), 4)
+  expect_message({
+    dat <- read_from_cache(file_name = "song.txt", verbose =  TRUE)
+  }, "Successfully")
+
+  expect_no_message({
+    dat2 <- read_from_cache(file_name = "song.txt", verbose =  FALSE)
+  })
+
+  expect_equal(dat, song)
+
 })
 
 
-# tools::R_user_dir("extractox", "cache")
+test_that("load cache fail if no file", {
+
+  expect_error({
+    read_from_cache(file_name = "non_esisto.txt", verbose =  TRUE)
+  })
+
+})
+
+# @@@@@@@@@@@@@@@@
+# Sandbox ----
+# @@@@@@@@@@@@@@@@
+
+
+test_that("Sandbox works", {
+
+  to_check <- normalizePath(tempdir(), mustWork = FALSE)
+
+  with_extr_sandbox(temp_dir = to_check, code = {
+    out <-  normalizePath(Sys.getenv("R_USER_CACHE_DIR"), mustWork = FALSE)
+  })
+
+  expect_equal(to_check, normalizePath(out, mustWork = FALSE))
+})
+
